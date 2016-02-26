04f0223158f4dee91d2003ad79333df00092f581
Andrew Burian <andrew@burian.ca>
2016-02-26 12:45:19 -0800

    Static content serving
    All unmatched root calls are directed to /static

--- server.go
+++ server.go
@@ -5,8 +5,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello, DIBS!")
+
+	// setup static content handler
+	http.Handle("/", http.FileServer(http.Dir("static/")))
+
+	// run the server
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
